55b60cbc5aa38fd69ea37f95e057eaafa53c412c
midhundas27 <dasmidhun27@gmail.com>
2024-04-09 16:39:55 +0530

    routing completed

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,304 +1,138 @@
 /** @format */
 "use client";
 
-import Container from "@/components/Container";
-import ForecastWeatherDetail from "@/components/ForecastWeatherDetail";
-import Navbar from "@/components/Navbar";
-import WeatherDetails from "@/components/WeatherDetails";
-import WeatherIcon from "@/components/WeatherIcon";
-import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
-import { convertWindSpeed } from "@/utils/convertWindSpeed";
-import { getDayOrNightIcon } from "@/utils/getDayOrNightIcon";
-import { metersToKilometers } from "@/utils/metersToKilometers";
-import CityTable from "./table";
-import axios from "axios";
-import { format, fromUnixTime, parseISO } from "date-fns";
-import { useQuery } from "react-query";
-import { loadingCityAtom, placeAtom } from "./atom";
-import { useAtom } from "jotai";
-import { useEffect } from "react";
+import React, { useState, useRef, useCallback } from 'react';
+import axios from 'axios';
+import SearchBox from "@/components/SearchBox";
+import { useInfiniteQuery } from 'react-query';
 
-interface WeatherDetail {
-  dt: number;
-  main: {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    sea_level: number;
-    grnd_level: number;
-    humidity: number;
-    temp_kf: number;
-  };
-  weather: {
-    id: number;
-    main: string;
-    description: string;
-    icon: string;
-  }[];
-  clouds: {
-    all: number;
-  };
-  wind: {
-    speed: number;
-    deg: number;
-    gust: number;
-  };
-  visibility: number;
-  pop: number;
-  sys: {
-    pod: string;
-  };
-  dt_txt: string;
+interface City {
+  geoname_id: string;
+  name: string;
+  cou_name_en: string;
+  timezone: string;
+  alternate_names?: string[];
+  ascii_name?: string;
 }
 
-interface WeatherData {
-  cod: string;
-  message: number;
-  cnt: number;
-  list: WeatherDetail[];
-  city: {
-    id: number;
-    name: string;
-    coord: {
-      lat: number;
-      lon: number;
-    };
-    country: string;
-    population: number;
-    timezone: number;
-    sunrise: number;
-    sunset: number;
-  };
+interface GeoData {
+  total_count: number;
+  results: City[];
 }
 
-export default function Home() {
-  const [place, setPlace] = useAtom(placeAtom);
-  const [loadingCity] = useAtom(loadingCityAtom);
+const CityTable = () => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const observer = useRef<IntersectionObserver | null>(null);
 
-  const { isLoading, error, data, refetch } = useQuery<WeatherData>(
-    "repoData",
-    async () => {
-      const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=5630f11436b4a3db363495ea648d062e&cnt=56`
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery<GeoData>(
+    ['cities', searchTerm], // Include searchTerm in the query key
+    async ({ pageParam = 0, queryKey }) => {
+      const [searchTerm] = queryKey;
+      const response = await axios.get<GeoData>(
+        `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=20&offset=${pageParam * 20}&q=${searchTerm}`
       );
-      return data;
+      return response.data;
+    },
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if (pages.length * 20 < lastPage.total_count) {
+          return pages.length;
+        } else {
+          return undefined;
+        }
+      },
     }
   );
+  
+  const lastCityRef = useCallback(
+    (node: HTMLElement | null) => { 
+      if (isFetchingNextPage) return;
 
-  useEffect(() => {
-    refetch();
-  }, [place, refetch]);
+      if (observer.current) {
+        observer.current.disconnect();
+      }
 
-  const firstData = data?.list[0];
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
 
-  // console.log("error", error);
+      if (node) {
+        observer.current.observe(node);
+      }
+    },
+    [isFetchingNextPage, fetchNextPage, hasNextPage]
+  );
 
-  console.log("data", data);
-
-  const uniqueDates = [
-    ...new Set(
-      data?.list.map(
-        (entry) => new Date(entry.dt * 1000).toISOString().split("T")[0]
-      )
+  const filteredCities = data?.pages.flatMap((page) =>
+    page.results.filter((city) =>
+      // city.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+       city.ascii_name?.toLowerCase().includes(searchTerm.toLowerCase())
+      // || city.alternate_names?.some((name) => name.toLowerCase().includes(searchTerm.toLowerCase()))
     )
-  ];
+  );
 
-  // Filtering data to get the first entry after 6 AM for each unique date
-  const firstDataForEachDate = uniqueDates.map((date) => {
-    return data?.list.find((entry) => {
-      const entryDate = new Date(entry.dt * 1000).toISOString().split("T")[0];
-      const entryTime = new Date(entry.dt * 1000).getHours();
-      return entryDate === date && entryTime >= 6;
-    });
-  });
+  const handleSearch = () => {
+    fetchNextPage({ pageParam: 0, search: searchTerm } as any);
+  };
+  
 
-  if (isLoading)
-    return (
-      <div className="flex items-center min-h-screen justify-center">
-        <p className="animate-bounce">Loading...</p>
-      </div>
-    );
-  if (error)
-    return (
-      <div className="flex items-center min-h-screen justify-center">
-        {/* @ts-ignore */}
-        <p className="text-red-400">{error.message}</p>
-      </div>
-    );
   return (
-    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
-      <Navbar location={data?.city.name} />
-      <CityTable />
-      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9  w-full  pb-10 pt-4 ">
-        {/* today data  */}
-        {loadingCity ? (
-          <WeatherSkeleton />
-        ) : (
-          <>
-            <section className="space-y-4 ">
-              <div className="space-y-2">
-                <h2 className="flex gap-1 text-2xl  items-end ">
-                  <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
-                  <p className="text-lg">
-                    ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
-                  </p>
-                </h2>
-                <Container className=" gap-10 px-6 items-center">
-                  {/* temprature */}
-                  <div className=" flex flex-col px-4 ">
-                    <span className="text-5xl">
-                      {convertKelvinToCelsius(firstData?.main.temp ?? 296.37)}°
-                    </span>
-                    <p className="text-xs space-x-1 whitespace-nowrap">
-                      <span> Feels like</span>
-                      <span>
-                        {convertKelvinToCelsius(
-                          firstData?.main.feels_like ?? 0
-                        )}
-                        °
-                      </span>
-                    </p>
-                    <p className="text-xs space-x-2">
-                      <span>
-                        {convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}
-                        °↓{" "}
-                      </span>
-                      <span>
-                        {" "}
-                        {convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}
-                        °↑
-                      </span>
-                    </p>
-                  </div>
-                  {/* time  and weather  icon */}
-                  <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
-                    {data?.list.map((d, i) => (
-                      <div
-                        key={i}
-                        className="flex flex-col justify-between gap-2 items-center text-xs font-semibold "
-                      >
-                        <p className="whitespace-nowrap">
-                          {format(parseISO(d.dt_txt), "h:mm a")}
-                        </p>
-
-                        {/* <WeatherIcon iconName={d.weather[0].icon} /> */}
-                        <WeatherIcon
-                          iconName={getDayOrNightIcon(
-                            d.weather[0].icon,
-                            d.dt_txt
-                          )}
-                        />
-                        <p>{convertKelvinToCelsius(d?.main.temp ?? 0)}°</p>
-                      </div>
-                    ))}
-                  </div>
-                </Container>
-              </div>
-              <div className=" flex gap-4">
-                {/* left  */}
-                <Container className="w-fit  justify-center flex-col px-4 items-center ">
-                  <p className=" capitalize text-center">
-                    {firstData?.weather[0].description}{" "}
-                  </p>
-                  <WeatherIcon
-                    iconName={getDayOrNightIcon(
-                      firstData?.weather[0].icon ?? "",
-                      firstData?.dt_txt ?? ""
-                    )}
-                  />
-                </Container>
-                <Container className="bg-yellow-300/80  px-6 gap-4 justify-between overflow-x-auto">
-                  <WeatherDetails
-                    visability={metersToKilometers(
-                      firstData?.visibility ?? 10000
-                    )}
-                    airPressure={`${firstData?.main.pressure} hPa`}
-                    humidity={`${firstData?.main.humidity}%`}
-                    sunrise={format(data?.city.sunrise ?? 1702949452, "H:mm")}
-                    // sunrise={}
-                    sunset={format(data?.city.sunset ?? 1702517657, "H:mm")}
-                    windSpeed={convertWindSpeed(firstData?.wind.speed ?? 1.64)}
-                  />
-                </Container>
-                {/* right  */}
-              </div>
-            </section>
-
-            {/* 7 day forcast data  */}
-            <section className="flex w-full flex-col gap-4  ">
-              <p className="text-2xl">Forcast (7 days)</p>
-              {firstDataForEachDate.map((d, i) => (
-                <ForecastWeatherDetail
-                  key={i}
-                  description={d?.weather[0].description ?? ""}
-                  weatehrIcon={d?.weather[0].icon ?? "01d"}
-                  date={d ? format(parseISO(d.dt_txt), "dd.MM") : ""}
-                  day={d ? format(parseISO(d.dt_txt), "dd.MM") : "EEEE"}
-                  feels_like={d?.main.feels_like ?? 0}
-                  temp={d?.main.temp ?? 0}
-                  temp_max={d?.main.temp_max ?? 0}
-                  temp_min={d?.main.temp_min ?? 0}
-                  airPressure={`${d?.main.pressure} hPa `}
-                  humidity={`${d?.main.humidity}% `}
-                  sunrise={format(
-                    fromUnixTime(data?.city.sunrise ?? 1702517657),
-                    "H:mm"
-                  )}
-                  sunset={format(
-                    fromUnixTime(data?.city.sunset ?? 1702517657),
-                    "H:mm"
-                  )}
-                  visability={`${metersToKilometers(d?.visibility ?? 10000)} `}
-                  windSpeed={`${convertWindSpeed(d?.wind.speed ?? 1.64)} `}
-                />
-              ))}
-            </section>
-          </>
-        )}
-      </main>
-      
+    <div className="container mx-auto px-4">
+      {/* <input
+        type="text"
+        placeholder="Search City"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      /> */}
+      <SearchBox
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onSubmit={handleSearch}
+              />
+      {/* <button onClick={handleSearch}>Search</button> */}
+      {/* {status === 'loading' && <p>Loading...</p>} */}
+      {filteredCities?.length === 0 && <p>No cities found</p>}
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead>
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">City Name</th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Country</th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Timezone</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {filteredCities?.map((city, index) => {
+            if (filteredCities.length === index + 1) {
+              return (
+                <tr key={city.geoname_id} ref={lastCityRef}>
+                  <td><a href={`/weather`} target="_blank" rel="noopener noreferrer">{city.name}</a></td>
+                  <td>{city.cou_name_en}</td>
+                  <td>{city.timezone}</td>
+                </tr>
+              );
+            } else {
+              return (
+                <tr key={city.geoname_id}>
+                  <td><a href={`/weather`} target="_blank" rel="noopener noreferrer">{city.name}</a></td>
+                  <td>{city.cou_name_en}</td>
+                  <td>{city.timezone}</td>
+                </tr>
+              );
+            }
+          })}
+        </tbody>
+      </table>
+      {/* {isFetchingNextPage && <p>Loading more...</p>} */}
     </div>
   );
-}
+};
 
-function WeatherSkeleton() {
-  return (
-    <section className="space-y-8 ">
-      {/* Today's data skeleton */}
-      <div className="space-y-2 animate-pulse">
-        {/* Date skeleton */}
-        <div className="flex gap-1 text-2xl items-end ">
-          <div className="h-6 w-24 bg-gray-300 rounded"></div>
-          <div className="h-6 w-24 bg-gray-300 rounded"></div>
-        </div>
-
-        {/* Time wise temperature skeleton */}
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          {[1, 2, 3, 4].map((index) => (
-            <div key={index} className="flex flex-col items-center space-y-2">
-              <div className="h-6 w-16 bg-gray-300 rounded"></div>
-              <div className="h-6 w-6 bg-gray-300 rounded-full"></div>
-              <div className="h-6 w-16 bg-gray-300 rounded"></div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* 7 days forecast skeleton */}
-      <div className="flex flex-col gap-4 animate-pulse">
-        <p className="text-2xl h-8 w-36 bg-gray-300 rounded"></p>
-
-        {[1, 2, 3, 4, 5, 6, 7].map((index) => (
-          <div key={index} className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
-            <div className="h-8 w-28 bg-gray-300 rounded"></div>
-            <div className="h-10 w-10 bg-gray-300 rounded-full"></div>
-            <div className="h-8 w-28 bg-gray-300 rounded"></div>
-            <div className="h-8 w-28 bg-gray-300 rounded"></div>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
-}
+export default CityTable;
